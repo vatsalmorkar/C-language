032cef4fa429f90fab54af586cf408c4e3e51c5a
meet2x <immeet4.0@gmail.com>
2024-08-29 18:09:31 +0530

    Add files via upload

---
+++ Project7.c
@@ -0,0 +1,16 @@
+// Redious formula LOL
+
+#include <stdio.h>
+#include <math.h>
+#define pi 3.14
+int main()
+{
+    float r, a;
+    printf("Enter the redious : ");
+    scanf(" %f", &r);
+
+    a = (pi * r * r);
+
+    printf("Your redius is : %f ", a);
+    return 0;
+}
