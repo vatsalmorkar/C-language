180d25e138c66120d0ca977a790941f48673a596
meet2x <immeet4.0@gmail.com>
2024-08-29 17:04:13 +0530

    Add files via upload

---
+++ Project1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+main()
+{
+   
+   printf("INTEREST RATE CALCULATION PROGRAM \n");
+   
+   int interestrate, amount, year;
+   int a, amount2;
+   a = 1;
+   printf ("Enter in rate : \n");
+   scanf ("%d", &interestrate);
+   printf ("Interestrate = %d \n", interestrate);
+
+   printf ("Enter in Amount : \n");
+   scanf ("%d", &amount);
+   printf ("Amount = %d \n", amount);
+
+   printf ("Enter Year : \n");
+   scanf ("%d", &year);
+   printf ("Year = %d \n", year);
+
+   while (a <= year)
+   {
+      amount2 = amount + interestrate * amount;
+      printf("Year %d Total Amount %d \n", a, amount2);
+      amount = amount2;
+      a ++;
+   }
+   
+}
