fb0a2024f59d15e219b4d332fe0d033d50f15cce
meet2x <immeet4.0@gmail.com>
2024-08-29 17:30:19 +0530

    Add files via upload

---
+++ Project2.c
@@ -0,0 +1,43 @@
+// use of the functions
+
+#include <stdio.h>
+
+    int func1 (int a, int b)
+     {
+        int c;
+        c = a + b;
+        return (c);
+     }
+
+    int func2 (int a, int b)
+     {
+        int c;
+        c = a - b;
+        return (c);
+     }
+ 
+int main()
+{
+    
+    printf("USE OF CALL FUNCTIONS \n");
+
+    int x, y, z, b;
+    
+        printf("Enter Value for x \n");
+        scanf("%d", &x);
+        printf("x = %d \n", x);
+
+        printf("Enter Value for y \n");
+        scanf("%d", &y);
+        printf("Y = %d \n", y);
+
+        
+    z = func1(x, y);
+    b = func2(x, y);
+
+    printf("The Ans Of z Plus = %d \n", z);
+    printf("The Ans Of b Minus = %d \n", b);
+
+}
+   
+
