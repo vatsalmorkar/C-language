f89e7c730cf86557f4b7770d626dd29a93e3ada1
meet2x <immeet4.0@gmail.com>
2024-08-29 17:36:37 +0530

    Project_2.c

--- Project2.c
+++ Project2.c
@@ -1,5 +1,6 @@
-// use of the functions
+//Use of the functions
 
+//Basic Syntex
 #include <stdio.h>
 
     int func1 (int a, int b)
@@ -39,5 +40,46 @@ int main()
     printf("The Ans Of b Minus = %d \n", b);
 
 }
-   
+/* #include <stdio.h>
+
+    int func1 (int a, int b);
+    int func2 (int a, int b);
+     
+int main()
+{
+    
+    printf("USE OF CALL FUNCTIONS \n");
+
+    int x, y, z, b;
+    
+        printf("Enter Value for x \n");
+        scanf("%d", &x);
+        printf("x = %d \n", x);
+
+        printf("Enter Value for y \n");
+        scanf("%d", &y);
+        printf("Y = %d \n", y);
+
+        
+    z = func1(x, y);
+    b = func2(x, y);
+
+    printf("The Ans Of z Plus = %d \n", z);
+    printf("The Ans Of b Minus = %d \n", b);
+
+}
+    int func1 (int a, int b)
+     {
+        int c;
+        c = a + b;
+        return (c);
+     }
+
+    int func2 (int a, int b)
+     {
+        int c;
+        c = a - b;
+        return (c);
+     }
+*/   
 
