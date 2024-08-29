809f53c7999a74df6f791cc56db45095775ec20e
meet2x <immeet4.0@gmail.com>
2024-08-29 17:20:24 +0530

    Project_1.c

--- Project1.c
+++ Project1.c
@@ -1,7 +1,8 @@
 //Interest Rate Calculator
 
+// Basc Program
 #include <stdio.h>
-main()
+int main()
 {
    
    printf("INTEREST RATE CALCULATION PROGRAM \n");
@@ -30,3 +31,32 @@ main()
    }
    
 }
+
+/*
+//2nd method
+#include <stdio.h>
+int main()
+{
+   
+   printf("INTEREST RATE CALCULATION PROGRAM \n");
+   
+   int interestrate, amount=0, year;
+   int a=1;
+   
+   printf ("Enter in rate : \n");
+   scanf ("%d", &interestrate);
+
+   printf ("Enter in Amount : \n");
+   scanf ("%d", &amount);
+
+   printf ("Enter Year : \n");
+   scanf ("%d", &year);
+
+   while (a <= year)
+   {
+      amount = amount + interestrate * amount;
+      printf("Year %d Total Amount %d \n", a, amount2);
+      a ++;
+   }
+   
+}*/
