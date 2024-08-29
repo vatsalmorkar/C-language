571488b6fe41c8b28da9fb24fdef6d466d549a2c
meet2x <immeet4.0@gmail.com>
2024-08-29 18:01:54 +0530

    Project_4.c

--- Project4.c
+++ Project4.c
@@ -6,12 +6,10 @@
     int a, b, c, d;
     printf("Enter the number of raws : \n");
     scanf(" %d", &a);
-    printf("Your input is = %d \n", a);
 
     printf("Enter the number of cell : \n");
     scanf(" %d", &b);
-    printf("Your input is = %d \n", b);
-
+    
         for (c = 1; c <= a; c++)
         {
             for (d = 1; d <= b; d++)
