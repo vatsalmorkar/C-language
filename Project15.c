aef3fab221db09c78ca4ab5fe629248988175ccf
meet2x <immeet4.0@gmail.com>
2024-08-31 14:55:22 +0530

    Add files via upload

---
+++ Project15.c
@@ -0,0 +1,18 @@
+//Write a program to read the price of an item in decimal form (  like 15.95 ) and print the output in paisa ( like 1595 paisa) .
+
+#include<stdio.h>
+
+int main()
+{
+    float x;
+    int y;
+
+    printf("Enter value to of rupees to convert in paisa : ");
+    scanf(" %f", &x);
+    //printf("Your Input = %f", x);
+
+    y = (int) (x * 100);
+
+    printf ("paisa =  %d", y);
+
+}
