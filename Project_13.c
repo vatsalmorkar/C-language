b3ed4b4083eaafad49964d8d03c8873d6d2680d7
meet2x <immeet4.0@gmail.com>
2024-08-31 14:45:05 +0530

    Project_13.c

--- Project_13.c
+++ Project_13.c
@@ -5,7 +5,7 @@
 
 int main ()
 {
-    int a;
+    int a, b;
     int x, y;
     char z[3];
 
@@ -17,6 +17,8 @@ int main ()
 
     x = a / 30;
     y = a % 30;
+    b = y-(x/2);
+
 
     /*if(strcmp(z,"yes")==0 || strcmp(z,"YES")==0)
     {
@@ -43,20 +45,34 @@ int main ()
     {
         if(a > 59)
         {
-        printf(" Months = %d \n ", x); 
-        printf("Days = %d \n", y-(x/2));
+        printf(" \n if starting month is January \n");
+        printf(" There are %d Months %d Days. \n", x,  y+1); 
+        printf("\n OR \n");
+
+        printf(" \n if starting month is Fabruary \n");
+        printf(" There are %d Months %d Days. \n", x,  b+2);
+        printf("\n OR \n");
+
+        printf(" \n if starting month are March - May - July - October - December \n");
+        printf(" There are %d Months %d Days. \n", x,  b-1);
+        printf("\n OR \n");
+
+        printf(" \n if starting month is April - Jun - Septembr - November\n");
+        printf(" There are %d Months %d Days. \n", x,  b);
         }
         else if (a < 59)
         {
-        printf(" Months = %d \n ", x); 
-        printf("Days = %d \n", y-1);
+        printf(" \n if starting month is January - April - Jun - Septembr - November  \n");
+        printf(" There are %d Months %d Days. \n", x,  y+1); 
+        printf("\n OR \n");
+        
         }
-        else if(a = 59)
+        /*else if(a = 59)
         {
         printf(" Months = 2 \n ");
         printf("Days = 0     OR\n");
         printf(" Months = %d \n ", x);  
-        }
+        }*/
     }
     else
     {
