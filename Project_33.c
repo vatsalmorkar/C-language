36604832ec5dde08af674e159dffb6738a56902e
meet2x <immeet4.0@gmail.com>
2024-09-04 18:31:57 +0530

    Project_33.c

---
+++ Project_33.c
@@ -0,0 +1,19 @@
+/ factorial calculation program. n! = n * (n-1) * (n-2) until (n-n) happen
+
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    int x = 1;
+    printf(" Enter The number :");
+    scanf(" %d", &n);
+
+    for( n ; n >= 1 ; n--)
+    {
+        x = x * n;
+    }
+
+    printf(" %d \n", x);
+    
+}
