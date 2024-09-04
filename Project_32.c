867fcf01f697c868fcdff6bed7b0f425c1f9a397
meet2x <immeet4.0@gmail.com>
2024-09-04 18:14:23 +0530

    Project_32.c

---
+++ Project_32.c
@@ -0,0 +1,32 @@
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int main()
+{
+    char x[10];
+    int count1, count2;
+    
+    while(1)
+    {
+        printf("input string : \n");
+        scanf("%10s", x);
+    
+        if (strcmp(x, "exit") == 0 || strcmp(x, "EXIT") == 0)
+        {
+            break;
+        }
+        else if (isdigit (x[0]))
+        {
+            count1++;
+        }
+        else if (isalpha (x[0]))
+        {
+            count2++;
+        }
+    }
+        printf("Total Digit Inputs are : %d \n", count1);
+        printf("Total Alpha Inputs are : %d \n", count2-1);
+        
+}
