4542ce2732d373a94b5100929d020b9443f08c67
meet2x <immeet4.0@gmail.com>
2024-09-09 19:36:15 +0530

    Prohect_55.c

--- Prohect_55.c
+++ Prohect_55.c
@@ -1,18 +1,19 @@
+// Triangular with floating value
 #include <stdio.h>
 
 int main()
 {
     int i, j;
-    int x = 1;
-    int z; // for store the modular result
+    float z = 0.1, g;
+
     for (i = 1; i <= 9; i++)
     {
         for (j = 1 ; j <= i ; j ++)
         {
-            printf(" %d", z);
-            z++;
+            printf(" %.1f", z);
+            g = z + 0.1;
+            z = g;
         }
         printf(" \n");
     }
-
 }
