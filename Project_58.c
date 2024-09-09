c29d4ffb9bf4e1557af65b8bb8546aa9c84d3cc7
meet2x <immeet4.0@gmail.com>
2024-09-10 00:12:52 +0530

    Project_58.c

--- Project_58.c
+++ Project_58.c
@@ -1,3 +1,4 @@
+// calculation from many numbers hoe many are divisible by 6 but not divisible nt 4
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -6,7 +7,7 @@ int main()
 {
     char a[5];
     int x, y, z;
-    int new_a, count1=0;
+    int new_a, count1=0, count2=0;
         while(1)
         {
             printf(" Enter The value between 0 to 100 :");
@@ -24,20 +25,19 @@ int main()
             }
             else
             {
-                //input[new_a] = 
+                x = new_a%2;
+                y = new_a%3;
+                z = new_a%4;
+
+                if (x == 0 && y == 0 && z != 0)
+                {
+                //printf("This number is divisible by 6 but not divisible by 4 : %d \n", new_a);
+                count2++;
+                } 
                 count1++;
             }
         }
-            printf(" Your total imputs are : %d", count1);
-
-            x = new_a%2;
-            y = new_a%3;
-            z = new_a%4;
-
-            if (x == 0 && y == 0 && z != 0)
-            {
-            printf("This number is divisible by 6 but not divisible by 4 : %d \n", new_a);
-            }
-
+            printf(" Your total imputs are : %d \n", count1);
+            printf(" \n Your total numbers which are divisible ny 6 not divisible by 4 are : %d", count2);
     
 }
