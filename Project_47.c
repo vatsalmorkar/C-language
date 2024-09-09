73bc29da48f3806fb43870a917358e000b7ba7a3
meet2x <immeet4.0@gmail.com>
2024-09-09 18:29:30 +0530

    Project_47.c

--- Project_47.c
+++ Project_47.c
@@ -1,20 +1,12 @@
-// Pay Slip Calculations withe different level of emplyee
+// Pay Slip Calculations with different level of emplyee
+
 #include<stdio.h>
 #include<string.h>
 #define BASIC_SALARY 2500
 
-int func1 (int conveyence, int entertainment)
-    {
-        int g_salary;
-        g_salary = BASIC_SALARY + conveyence + entertainment;
-        return(g_salary);
-    }
-float func2 (int g_salary, float tax_rate)
-    {
-        float net_income;
-        net_income = g_salary - (g_salary * tax_rate);
-        return(net_income);
-    }
+int func1 (int conveyence, int entertainment);
+float func2 (int g_salary, float tax_rate);
+
 int main()
 {
     char n[8], n1[20], n2[20]; // To store the name
@@ -29,21 +21,31 @@ int main()
     if(strcmp(n,"meet") == 0 || strcmp(n,"MEET") == 0)
     {
         int a=250, b=0;
-        printf(" You are  level 4 emplyee \n");
+        printf("\n You are  level 4 emplyee \n");
         // for total income
         l4 = func1 (a, b);
-        printf("Your Total income is : %d \n", l4);
+        printf(" \n Your Total income is : %d \n", l4);
         // for net income
         if(l4 <= 2000)
         {
             l4_2 = func2(l4, 1);
-            printf("Your Net income is : %.2f", l4_2);
+            printf(" \n Your Net income is : %.2f", l4_2);
         }
         else if (2000 < l4 <= 4000)
         {
             l4_2 = func2(l4, 0.30);
         }
-    }
-
-    
+    }   
 }
+int func1 (int conveyence, int entertainment)
+    {
+        int g_salary;
+        g_salary = BASIC_SALARY + conveyence + entertainment;
+        return(g_salary);
+    }
+float func2 (int g_salary, float tax_rate)
+    {
+        float net_income;
+        net_income = g_salary - (g_salary * tax_rate);
+        return(net_income);
+    }
