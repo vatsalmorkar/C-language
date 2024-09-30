287b78afb5b9d6a533f9244b67511ef964dbe293
meet <immeet4.0@gmail.com>
2024-09-30 19:16:50 +0530

    Add files via upload

---
+++ temp.c
@@ -0,0 +1,45 @@
+// Hollow triangle
+
+#include<stdio.h>
+
+int main()
+{
+  int i, j;
+
+  for (i=1 ; i<=10 ; i++)
+  {
+    if(i == 1)
+     {
+      for (j=1 ; j<=10 ; j++)
+      {
+        printf(" * ");
+      }
+     }
+    else if (i == 10)
+     {
+      for (j=1 ; j<=10 ; j++)
+      {
+        printf(" * ");
+      }
+     }
+    else
+     {
+      for (j=1 ; j<=10 ; j++)
+      {
+        if( j == 1 )
+        {
+          printf(" * ");
+        }
+        else if (j == 10)
+        {
+          printf(" * ");
+        }
+        else
+        {
+          printf("   ");
+        }
+      }
+     }
+    printf(" \n");
+  }
+}
