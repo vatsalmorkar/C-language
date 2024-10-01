f2240f483dd9b8c42f96c826c64d38035bb56535
meet <immeet4.0@gmail.com>
2024-10-01 17:40:32 +0530

    Project_66.c

--- Project_66.c
+++ Project_66.c
@@ -9,37 +9,37 @@ int main()
   for (i=1 ; i<=10 ; i++)
   {
     if(i == 1)
-     {
-      for (j=1 ; j<=10 ; j++)
       {
-        printf(" * ");
+      for (j=1 ; j<=10 ; j++)
+        {
+          printf(" * ");
+        }
       }
-     }
     else if (i == 10)
      {
       for (j=1 ; j<=10 ; j++)
-      {
-        printf(" * ");
-      }
+        {
+          printf(" * ");
+        }
      }
     else
      {
       for (j=1 ; j<=10 ; j++)
-      {
-        if( j == 1 )
         {
-          printf(" * ");
-        }
-        else if (j == 10)
-        {
-          printf(" * ");
-        }
-        else
-        {
-          printf("   ");
+          if( j == 1 )
+          {
+            printf(" * ");
+          }
+          else if (j == 10)
+          {
+            printf(" * ");
+          }
+          else
+          {
+            printf("   ");
+          }
         }
       }
-     }
     printf(" \n");
   }
 }
