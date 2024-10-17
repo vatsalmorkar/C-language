af54927cc5c74fb3de9e96c2c6bc860f2da71e03
meet <immeet4.0@gmail.com>
2024-10-17 13:58:51 +0530

    Find Max And Min Value From Array

---
+++ Project_76.c
@@ -0,0 +1,33 @@
+// Find the maximum and minimum value from given array
+
+#include<stdio.h>
+
+int main()
+{
+  int x, value1, max=-9999, min=9999;
+  printf(" \n Howmany values you want to enter in to the array \n");
+  scanf("%d", &x);
+
+  int array1[x+1];
+
+  for(int i=0 ; i<x ; i++)
+  {
+    printf("Enter Value %d : ", i+1);
+    scanf(" %d", &array1[i]);
+  }
+
+  for(int i=0 ; i<x ; i++)
+  {
+    if(array1[i]<min)
+    {
+      min = array1[i];
+    }
+    if(array1[i]>max)
+    {
+      max = array1[i];
+    }
+  }
+  printf(" Min Value =  %d \n", min);
+  printf(" Max Value =  %d", max);
+  return 0;
+}
