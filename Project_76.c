aec6b56508969adffe1e7193079e0030166a1e98
meet <immeet4.0@gmail.com>
2024-10-17 19:36:12 +0530

    Find max and min from array

--- Project_76.c
+++ Project_76.c
@@ -19,7 +19,7 @@ int main()
 
   for(int i=0 ; i<x ; i++)
   {
-    // i can compare with the first element to get the result
+    // i can compare with the first element to get the result //
     // max = min = array1[0]
     if(array1[i]<min)
     {
